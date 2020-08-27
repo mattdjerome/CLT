428b254adf3925db139ea9e96db288f8ea1c6b97
mattdjerome <mattdjerome@gmail.com>
2020-08-27 09:02:19 -0700

    contains multiple commands with basic logging

--- macos_clt.sh
+++ macos_clt.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
 #Check if a script is being run using sudo
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root. Re-launch using the sudo command." 
-   exit 1
-fi
+# if [[ $EUID -ne 0 ]]; then
+#    echo "This script must be run as root. Re-launch using the sudo command." 
+#    exit 1
+# fi
 
-echo "Please choose a command:"
-options=("Renew Automated Device Enrollment" "Monitor Network Traffic" "Deactivate Software Updates" "Quit")
-select opt in "${options[@]}"
-do
-case $opt in
-        "Renew Automated Device Enrollment")
-            /usr/bin/proflies -renew type enrollment
-            ;;
-        "Monitor Network Traffic")
-            nettop
-            ;;
-        "Deactivate Software Updates")
-            /usr/sbin/softwareupdate --schedule off
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+#Check if Log File exists
+function LOG {
+		echo "" 
+		date 
+		echo "--------------------------" 
+		echo "Selected Command is $cat_sel. Executing"
+		if [[ $? = 0 ]]; then
+			echo "Command executed successfully."
+		else
+			echo "Command encountered an error"
+		fi
+}
+FILE=/Library/Logs/macos_clt.log
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+	touch $FILE
+	echo "Log file created at $FILE"
+fi
+#trying it out using applescript dropdown
+function category {
+cat_sel=$(osascript -e 'choose from list {"Show Hidden Files", "Hide Hidden Files", "list folder contents", "Jamf Inventory", "Jamf Policy-Check", "Jamf Configuration Profile Check", "Run ADP Enrollment" } with title "Commands" with prompt "Please choose"')
+echo "The selected category is $cat_sel"
+}
+category
+
+case $cat_sel in
+	"Show Hidden Files")
+		defaults write com.apple.finder AppleShowAllFiles -bool TRUE >> $FILE
+		killall -KILL Finder >> $FILE
+		LOG >> $FILE
+		;;
+	"list folder contents" )
+		ls
+		LOG >> $FILE
+		;;
+esac
+
